051ad0580d763db11d6b57dbb977225068d2835d
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-17 13:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function volume_sphere() {
     //Write your code here
-   var volume;
-  var radius = document.getElementById('radius').value;
-  radius = Math.abs(radius);
-  volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  volume = volume.toFixed(4);
-  document.getElementById('volume').value = volume;
-  return false;
+   const radius = parseFloat(document.getElementById("radius").value);	
+	radius = Math.abs(radius);
+   const volume = (4/3)*Math.PI*Math.pow(radius, 3);
+   document.getElementById("volume").value = volume.toFixed(5);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
